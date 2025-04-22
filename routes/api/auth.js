f4bc0d74abbbd6939bb165b54a56a41659e31476
kayla <kayla@exmaple.com>
2025-04-22 05:04:26 -0400

    Final Fix5

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,5 +1,5 @@
 import express from "express";
-// import bcrypt from "bcrypt";
+import bcrypt from "bcrypt";
 import crypto from "crypto";
 import { jwtDecode } from "jwt-decode";
 import jwt from "jsonwebtoken";
